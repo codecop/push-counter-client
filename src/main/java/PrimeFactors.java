0616cd5a2f93938a066bd908095a7ee4ce0cb6cf
Peter Kofler <peter.kofler@code-cop.org>
2021-02-07 23:34:12 +0100

    cleanup

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -6,10 +6,11 @@ public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> factors = new ArrayList<>();
 
-        for (int candidate = 2; candidate <= 3; candidate++)
-        while (n % candidate == 0) {
-            factors.add(candidate);
-            n /= candidate;
+        for (int candidate = 2; candidate <= 3; candidate++) {
+            while (n % candidate == 0) {
+                factors.add(candidate);
+                n /= candidate;
+            }
         }
 
         if (n > 1) {
