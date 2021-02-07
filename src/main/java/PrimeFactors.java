11d959d1beda5b9e559101f1e202435b6ac3a6c1
Peter Kofler <peter.kofler@code-cop.org>
2021-02-07 23:32:14 +0100

    Refactor.

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -5,13 +5,18 @@ public class PrimeFactors {
 
     public static List<Integer> generate(int n) {
         List<Integer> factors = new ArrayList<>();
-        while (n % 2 == 0) {
-            factors.add(2);
-            n /= 2;
+
+        int candidate = 2;
+
+        while (n % candidate == 0) {
+            factors.add(candidate);
+            n /= candidate;
         }
+
         if (n > 1) {
             factors.add(n);
         }
+
         return factors;
     }
 
