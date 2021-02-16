f7fcef5048d194afd40da8139178831aa9885214
Johannes Link <business@johanneslink.net>
2021-02-16 11:59:52 +0100

    Introduce loadFactor parameter

--- src/main/java/parrot/ParrotTypeEnum.java
+++ src/main/java/parrot/ParrotTypeEnum.java
@@ -4,12 +4,12 @@ public enum ParrotTypeEnum {
 
 	EUROPEAN {
 		@Override
-		public double getSpeed(double baseSpeed) {
+		public double getSpeed(double baseSpeed, double loadFactor) {
 			return baseSpeed;
 		}
 	}, AFRICAN, NORWEGIAN_BLUE;
 
-	public double getSpeed(double baseSpeed) {
+	public double getSpeed(double baseSpeed, double loadFactor) {
 		throw new RuntimeException("Should be unreachable");
 	}
 
