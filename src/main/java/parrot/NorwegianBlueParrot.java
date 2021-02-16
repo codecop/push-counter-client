001448c5e6e5f33d2f5e8d29da6a3cefaf954e10
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 13:17:31 +0100

    NorwegianBlueParrot remove super call

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -6,7 +6,6 @@ public class NorwegianBlueParrot extends Parrot {
     private final boolean isNailed;
 
     public NorwegianBlueParrot(final double voltage, final boolean isNailed) {
-        super();
         this.voltage = voltage;
         this.isNailed = isNailed;
     }
