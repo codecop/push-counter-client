ea9c1a10706d96ee14283fc57fc5e6931e419d9b
Pavol Cesek <pavol.cesek@ilogics.com>
2021-02-16 13:42:02 +0100

    Remove type from parrot

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -4,12 +4,9 @@ public class Parrot {
 
     private ParrotInt parrot;
 
-    private ParrotTypeEnum type;
-
 
     public Parrot(ParrotInt parrot) {
         this.parrot = parrot;
-        this.type = parrot.getType();
     }
 
     public double getSpeed() {
