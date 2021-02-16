c9e5dc3f909f67b771ea26052c1d88abdd906727
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 13:10:23 +0100

    ParrotTypeEnum type field removed

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -2,10 +2,7 @@ package parrot;
 
 public class Parrot {
 
-    private final ParrotTypeEnum type;
-
     public Parrot(ParrotTypeEnum type) {
-        this.type = type;
     }
 
     public double getSpeed() {
