9ec7723682d063a247d6d19324409630fed0103a
Pavol Cesek <pavol.cesek@ilogics.com>
2021-02-16 13:22:23 +0100

    Remove numberOfCoconuts arg from constructor

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -8,7 +8,7 @@ public class Parrot {
     private double voltage;
     private boolean isNailed;
 
-    public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+    public Parrot(ParrotTypeEnum type, double voltage, boolean isNailed) {
         this.type = type;
         this.voltage = voltage;
         this.isNailed = isNailed;
--- src/test/java/parrot/ParrotTest.java
+++ src/test/java/parrot/ParrotTest.java
@@ -32,19 +32,19 @@ public class ParrotTest {
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, true);
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 1.5, true);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 1.5, false);
         assertEquals(18.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
