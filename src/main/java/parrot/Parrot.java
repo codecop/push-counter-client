f6623919021807fe4339d3747ece8b13e1684c9e
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 12:07:06 +0100

    remove field voltage

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -3,12 +3,10 @@ package parrot;
 public class Parrot {
 
     private ParrotTypeEnum type;
-    private double voltage;
     private boolean isNailed;
 
     public Parrot(ParrotTypeEnum type, double voltage, boolean isNailed) {
         this.type = type;
-        this.voltage = voltage;
         this.isNailed = isNailed;
     }
 
