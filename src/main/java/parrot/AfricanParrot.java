1e42b048f46e3b7fcf0875380b1f881ebb6acb05
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:40:43 +0100

    move getLoadFactor

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -13,4 +13,8 @@ public class AfricanParrot extends Parrot {
         return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
     }
 
+    protected double getLoadFactor() {
+        return 9.0;
+    }
+
 }
--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -4,11 +4,6 @@ public abstract class Parrot {
 
     public abstract double getSpeed();
 
-
-    protected double getLoadFactor() {
-        return 9.0;
-    }
-
     protected double getBaseSpeed() {
         return 12.0;
     }
