46f1089c743478730b02c39a5d79d998159b042a
Johannes Link <business@johanneslink.net>
2021-02-16 12:03:16 +0100

    Move AFRICAN branch to ParrotTypeEnum

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -16,8 +16,6 @@ public class Parrot {
 
     public double getSpeed() {
         switch (type) {
-        case AFRICAN:
-            return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
         case NORWEGIAN_BLUE:
             return (isNailed) ? 0 : getBaseSpeed(voltage);
         default:
--- src/main/java/parrot/ParrotTypeEnum.java
+++ src/main/java/parrot/ParrotTypeEnum.java
@@ -7,7 +7,12 @@ public enum ParrotTypeEnum {
 		public double getSpeed(double baseSpeed, double loadFactor, int numberOfCoconuts, boolean isNailed) {
 			return baseSpeed;
 		}
-	}, AFRICAN, NORWEGIAN_BLUE;
+	}, AFRICAN {
+		@Override
+		public double getSpeed(double baseSpeed, double loadFactor, int numberOfCoconuts, boolean isNailed) {
+			return Math.max(0, baseSpeed - loadFactor * numberOfCoconuts);
+		}
+	}, NORWEGIAN_BLUE;
 
 	public double getSpeed(double baseSpeed, double loadFactor, int numberOfCoconuts, boolean isNailed) {
 		throw new RuntimeException("Should be unreachable");
