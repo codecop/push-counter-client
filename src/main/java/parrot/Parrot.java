7d9b4cd1115a5bfd2faf0dda0f65a3411fde3189
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 13:05:46 +0100

    remove African switch

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -12,8 +12,6 @@ public class Parrot {
         switch (type) {
             case EUROPEAN:
                 return getBaseSpeed();
-            case AFRICAN:
-                return getBaseSpeed();
         }
         return getBaseSpeed();
     }
