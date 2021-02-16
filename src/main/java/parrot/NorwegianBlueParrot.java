694281f9aea25bdf41231a020c9eb1d1863a9785
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 12:03:52 +0100

    voltage to norvegian

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -2,8 +2,11 @@ package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
 
+    private double voltage;
+
     public NorwegianBlueParrot(final ParrotTypeEnum type, final double voltage, final boolean isNailed) {
         super(type, voltage, isNailed);
+        this.voltage = voltage;
     }
 
 }
