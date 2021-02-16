df15d7523908ec627c38dc5a8f2922d9e416c94d
Pavol Cesek <pavol.cesek@ilogics.com>
2021-02-16 13:46:08 +0100

    Replate parrot with parrotint in tests

--- src/test/java/parrot/ParrotTest.java
+++ src/test/java/parrot/ParrotTest.java
@@ -8,43 +8,43 @@ public class ParrotTest {
 
     @Test
     public void getSpeedOfEuropeanParrot() {
-        Parrot parrot = new Parrot(new EuropeanParrot());
+        ParrotInt parrot = new EuropeanParrot();
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        Parrot parrot = new Parrot(new AfricanParrot(1));
+        ParrotInt parrot = new AfricanParrot(1);
         assertEquals(3.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        Parrot parrot = new Parrot(new AfricanParrot(2));
+        ParrotInt parrot = new AfricanParrot(2);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_No_Coconuts() {
-        Parrot parrot = new Parrot(new AfricanParrot(0));
+        ParrotInt parrot = new AfricanParrot(0);
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
-        Parrot parrot = new Parrot(new NorwegianBlueParrot(1.5, true));
+        ParrotInt parrot = new NorwegianBlueParrot(1.5, true);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-        Parrot parrot = new Parrot(new NorwegianBlueParrot(1.5, false));
+        ParrotInt parrot = new NorwegianBlueParrot(1.5, false);
         assertEquals(18.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        Parrot parrot = new Parrot(new NorwegianBlueParrot(4, false));
+        ParrotInt parrot = new NorwegianBlueParrot(4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
