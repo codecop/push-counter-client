4e25cbe900ab528293674237293ed850cfdc7e1b
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:07:21 +0100

    extract method

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -21,10 +21,14 @@ public class Parrot {
         case AFRICAN:
             return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
         case NORWEGIAN_BLUE:
-            return (isNailed) ? 0 : getBaseSpeed(voltage);
+            return getNorwegianParrotSpeed();
         default:
             throw new RuntimeException("Should be unreachable");
         }
+    }
+
+    private double getNorwegianParrotSpeed() {
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 
     protected double getBaseSpeed(double voltage) {
