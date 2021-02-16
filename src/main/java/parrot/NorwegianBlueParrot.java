f027abae9df904f51c1ecbe1ab1a44078a9977da
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:19:20 +0100

    make fields final

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -2,8 +2,8 @@ package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
 
-    private double voltage;
-    private boolean isNailed;
+    private final double voltage;
+    private final boolean isNailed;
 
     public NorwegianBlueParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
         super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts);
