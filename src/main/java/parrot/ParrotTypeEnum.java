981b282305647a8c6a23c216be0bf2c857386673
Johannes Link <business@johanneslink.net>
2021-02-16 13:06:47 +0100

    use ParrotTypeEnum.getBaseSpeed()

--- src/main/java/parrot/ParrotTypeEnum.java
+++ src/main/java/parrot/ParrotTypeEnum.java
@@ -5,17 +5,17 @@ public enum ParrotTypeEnum {
 	EUROPEAN {
 		@Override
 		public double getSpeed(double baseSpeed, double loadFactor, double voltage, int numberOfCoconuts, boolean isNailed) {
-			return baseSpeed;
+			return this.getBaseSpeed();
 		}
 	}, AFRICAN {
 		@Override
 		public double getSpeed(double baseSpeed, double loadFactor, double voltage, int numberOfCoconuts, boolean isNailed) {
-			return Math.max(0, baseSpeed - loadFactor * numberOfCoconuts);
+			return Math.max(0, this.getBaseSpeed() - loadFactor * numberOfCoconuts);
 		}
 	}, NORWEGIAN_BLUE {
 		@Override
 		public double getSpeed(double baseSpeed, double loadFactor, double voltage, int numberOfCoconuts, boolean isNailed) {
-			return (isNailed) ? 0 : Math.min(24.0, voltage * baseSpeed);
+			return (isNailed) ? 0 : Math.min(24.0, voltage * this.getBaseSpeed());
 		}
 	};
 
@@ -23,11 +23,11 @@ public enum ParrotTypeEnum {
 		throw new RuntimeException("Should be unreachable");
 	}
 
-	private double getLoadFactor() {
+	protected double getLoadFactor() {
 		return 9.0;
 	}
 
-	private double getBaseSpeed() {
+	protected double getBaseSpeed() {
 		return 12.0;
 	}
 
