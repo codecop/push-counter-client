4cebd8c891bcba86da3b12f2c15fb3378966ba55
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:16:02 +0100

    remove methods

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -20,15 +20,9 @@ public class Parrot {
             return getBaseSpeed();
         case AFRICAN:
             return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-        case NORWEGIAN_BLUE:
-            return getNorwegianParrotSpeed();
         default:
             throw new RuntimeException("Should be unreachable");
         }
-    }
-
-    private double getNorwegianParrotSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 
     protected double getBaseSpeed(double voltage) {
