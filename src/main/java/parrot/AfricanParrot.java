b670c29aafc0e444a25523f93d2d11e86d9efe4d
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:25:26 +0100

    add field to african parrot

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -1,7 +1,11 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
+
+    private final int numberOfCoconuts;
+
     public AfricanParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
         super(ParrotTypeEnum.AFRICAN, numberOfCoconuts);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 }
