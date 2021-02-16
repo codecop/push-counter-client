c82dcc6238bd8289f2176f01f2d3038fb970ffa8
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 11:54:23 +0100

    African parrot

---
+++ src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+    public AfricanParrot(final ParrotTypeEnum type, final int numberOfCoconuts, final double voltage, final boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
