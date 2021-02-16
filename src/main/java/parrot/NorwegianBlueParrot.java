71f5f36bcfa83ba7a677ffe3809a9f68d34d5599
Peter Kudela <peter.kudela@cargo-partner.com>
2021-02-16 12:04:19 +0100

    isNailed

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -3,10 +3,12 @@ package parrot;
 public class NorwegianBlueParrot extends Parrot {
 
     private double voltage;
+    private boolean isNailed;
 
     public NorwegianBlueParrot(final ParrotTypeEnum type, final double voltage, final boolean isNailed) {
         super(type, voltage, isNailed);
         this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
 }
