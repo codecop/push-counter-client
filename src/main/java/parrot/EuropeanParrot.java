50eb3ca30cd9219c9169d01288de9412e16fb5d0
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 11:51:47 +0100

    european parrot

---
+++ src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,9 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot {
+
+    public EuropeanParrot(final ParrotTypeEnum type, final int numberOfCoconuts, final double voltage, final boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+}
