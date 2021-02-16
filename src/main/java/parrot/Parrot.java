2a8ee266897812b5acc1db80a209710f58a60f49
Johannes Link <business@johanneslink.net>
2021-02-16 13:03:59 +0100

    Remove switch

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -2,31 +2,28 @@ package parrot;
 
 public class Parrot {
 
-    private ParrotTypeEnum type;
-    private int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
+	private ParrotTypeEnum type;
+	private int numberOfCoconuts;
+	private double voltage;
+	private boolean isNailed;
 
-    public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        this.type = type;
-        this.numberOfCoconuts = numberOfCoconuts;
-        this.voltage = voltage;
-        this.isNailed = isNailed;
-    }
+	public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+		this.type = type;
+		this.numberOfCoconuts = numberOfCoconuts;
+		this.voltage = voltage;
+		this.isNailed = isNailed;
+	}
 
-    public double getSpeed() {
-        switch (type) {
-        default:
-            return type.getSpeed(getBaseSpeed(), getLoadFactor(), voltage, numberOfCoconuts, isNailed);
-        }
-    }
+	public double getSpeed() {
+		return type.getSpeed(getBaseSpeed(), getLoadFactor(), voltage, numberOfCoconuts, isNailed);
+	}
 
-    private double getLoadFactor() {
-        return 9.0;
-    }
+	private double getLoadFactor() {
+		return 9.0;
+	}
 
-    private double getBaseSpeed() {
-        return 12.0;
-    }
+	private double getBaseSpeed() {
+		return 12.0;
+	}
 
 }
