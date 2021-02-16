bb6156b679e4c2da2741ae919de7d1e4017790c7
Raphael Loeffler <raphael.loeffler@cargo-partner.com>
2021-02-16 13:08:32 +0100

    copy properties

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,7 +1,13 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
+
+    private double voltage;
+    private boolean isNailed;
+
     public NorwegianBlueParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
         super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
+        this.isNailed = isNailed;
+        this.voltage = voltage;
     }
 }
