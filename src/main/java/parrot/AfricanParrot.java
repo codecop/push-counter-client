73c2f94f044a03a91e289be511c3d93f0f15b332
Krisztian Szeles <krisztian.szeles@ilogics.com>
2021-02-17 11:34:38 +0100

    Overriding getSpeed in AfricanParrot

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -1,7 +1,16 @@
 package parrot;
 
 public class AfricanParrot extends Parrot{
+    private int numberOfCoconuts;
+
     public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
 }
