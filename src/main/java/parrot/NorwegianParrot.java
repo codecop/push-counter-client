d2347ccbf08e95cf8cb2c02aec16e4042e2d0062
pn13057 <anca0>
2021-02-17 11:23:54 +0100

    NorwegianParrot

---
+++ src/main/java/parrot/NorwegianParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class NorwegianParrot extends Parrot {
+    public NorwegianParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
