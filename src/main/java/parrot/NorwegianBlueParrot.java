80161f4ef9e538d325f27dea2cce42c0aaa78b6f
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 11:47:18 +0100

    Create NorwegianBlueParrot class

---
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot {
+    public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
