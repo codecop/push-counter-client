d7ca8c5ee7ca0e96eeec347d13c935da522fab5e
pn13057 <anca0>
2021-02-17 11:42:02 +0100

    move number of coconuts to african

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -1,8 +1,12 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
+
+    private int numberOfCoconuts;
+
     public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         super(numberOfCoconuts, voltage, isNailed);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 
     @Override
