35970ea71812518b194ae08b4e25f2fe69bc9285
Krisztian Szeles <krisztian.szeles@ilogics.com>
2021-02-17 13:45:56 +0100

    Refactoring AfricanParrot

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -1,7 +1,6 @@
 package parrot;
 
 public class AfricanParrot implements Parrot{
-    private static final double LOAD_FACTOR = 9.0;
     private static final int ZERO = 0;
     private final int numberOfCoconuts;
 
