8254cddf068519a23023d34923f3e1b4a2238ea0
Krisztian Szeles <krisztian.szeles@ilogics.com>
2021-02-17 11:36:31 +0100

    Overriding getSpeed in NorwegianBlueParrot

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,7 +1,17 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot{
+    private boolean isNailed;
+    private double voltage;
+
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+        this.isNailed = isNailed;
+        this.voltage = voltage;
+    }
+
+    @Override
+    public double getSpeed(){
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 }
