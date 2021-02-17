6ab8b8674bff1fdd0a2806b06ea8030fee0cdf0f
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 13:32:46 +0100

    Add parrotVoltage to NorwegianBlueParrot

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,8 +1,12 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
+
+    private final double parrotVoltage;
+
     public NorwegianBlueParrot(final double voltage, final boolean isNailed) {
         super(voltage, isNailed);
+        this.parrotVoltage = voltage;
     }
 
     @Override
