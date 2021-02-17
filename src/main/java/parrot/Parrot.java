1f53e735d7161e35cb3ed88dfb49908fb35d74f3
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 11:40:21 +0100

    Remove AfricanParrot case in Parrot getSpeed

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -18,8 +18,6 @@ public class Parrot {
         switch (type) {
             case EUROPEAN:
                 return getBaseSpeed();
-            case AFRICAN:
-                return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE:
                 return (isNailed) ? 0 : getBaseSpeed(voltage);
             default:
