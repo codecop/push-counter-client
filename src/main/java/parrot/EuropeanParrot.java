89c7db78d682cbde70d27d246ce27ac3afaf97c8
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 13:30:35 +0100

    Delete parameter numberOfCoconuts in EuropeanParrot constructor

--- src/main/java/parrot/EuropeanParrot.java
+++ src/main/java/parrot/EuropeanParrot.java
@@ -1,7 +1,7 @@
 package parrot;
 
 public class EuropeanParrot extends Parrot {
-    public EuropeanParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
+    public EuropeanParrot(final double voltage, final boolean isNailed) {
         super(voltage, isNailed);
     }
 
