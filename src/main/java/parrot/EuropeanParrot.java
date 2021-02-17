03b0b78a0d797e71a2c4396ba304a039369d8aa1
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 11:44:18 +0100

    Overwrite getSpeed in EuropeanParrot

--- src/main/java/parrot/EuropeanParrot.java
+++ src/main/java/parrot/EuropeanParrot.java
@@ -4,4 +4,9 @@ public class EuropeanParrot extends Parrot {
     public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return getBaseSpeed();
+    }
 }
