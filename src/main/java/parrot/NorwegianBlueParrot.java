b2f403df5cf341e060ade9f7e6d908bbff305e0f
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 13:38:06 +0100

    Add nailed to NorwegianBlueParrot

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -3,10 +3,12 @@ package parrot;
 public class NorwegianBlueParrot extends Parrot {
 
     private final double voltage;
+    private final boolean nailed;
 
     public NorwegianBlueParrot(final double voltage, final boolean isNailed) {
         super(isNailed);
         this.voltage = voltage;
+        this.nailed = isNailed;
     }
 
     @Override
