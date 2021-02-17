b7d39a1940c36c92c5e9d893fd7b4bf7688aec0d
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 13:21:06 +0100

    Make Parameters in NorwegianBlueParrot constructor final

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,7 +1,7 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
-    public NorwegianBlueParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+    public NorwegianBlueParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
         super(numberOfCoconuts, voltage, isNailed);
     }
 
