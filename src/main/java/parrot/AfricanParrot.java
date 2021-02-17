1cc211c7e8ac0aceaef73af0eeb8f56ea96200fd
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 11:32:39 +0100

    Create AfricanParrot

---
+++ src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,10 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+
+    public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+
+}
