6b4705cc7f578ce5c96c1a06b4a104c5a1c1482f
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 11:24:24 +0100

    Reformat code

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -16,14 +16,14 @@ public class Parrot {
 
     public double getSpeed() {
         switch (type) {
-        case EUROPEAN:
-            return getBaseSpeed();
-        case AFRICAN:
-            return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-        case NORWEGIAN_BLUE:
-            return (isNailed) ? 0 : getBaseSpeed(voltage);
-        default:
-            throw new RuntimeException("Should be unreachable");
+            case EUROPEAN:
+                return getBaseSpeed();
+            case AFRICAN:
+                return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+            case NORWEGIAN_BLUE:
+                return (isNailed) ? 0 : getBaseSpeed(voltage);
+            default:
+                throw new RuntimeException("Should be unreachable");
         }
     }
 
