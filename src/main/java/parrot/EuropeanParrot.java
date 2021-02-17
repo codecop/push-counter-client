6a1df36563b651dad14627f7b5a8c66b895c1cdc
pn13057 <anca0>
2021-02-17 11:22:28 +0100

    EuropeanParrot

---
+++ src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,8 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot{
+
+    public EuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
