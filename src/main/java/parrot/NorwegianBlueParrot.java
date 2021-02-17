9de4aca285932b20c574bca6fcf993a2fd81bf66
Krisztian Szeles <krisztian.szeles@ilogics.com>
2021-02-17 13:33:04 +0100

    Creating a constant

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,17 +1,19 @@
 package parrot;
 
 public class NorwegianBlueParrot implements Parrot{
+    public static final int ZERO = 0;
     private final boolean isNailed;
     private final double voltage;
 
     public NorwegianBlueParrot(double voltage, boolean isNailed) {
         this.isNailed = isNailed;
         this.voltage = voltage;
+
     }
 
     @Override
     public double getSpeed(){
-        return (isNailed) ? 0 : getBaseSpeed();
+        return (isNailed) ? ZERO : getBaseSpeed();
     }
 
     private double getBaseSpeed() {
