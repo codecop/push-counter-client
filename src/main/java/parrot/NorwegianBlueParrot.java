34fb4a852a5503fe7d57317c4b62b9cba4cc6cf3
Hauser Florian <hauserflorian@yahoo.de>
2021-02-17 13:06:49 +0100

    Overwrite getSpeed in NorwegianBlueParrot class

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -4,4 +4,9 @@ public class NorwegianBlueParrot extends Parrot {
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return (isNailed()) ? 0 : getBaseSpeed(getVoltage());
+    }
 }
