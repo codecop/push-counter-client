cfa14e9bf5d3f5805a2cd28e4567b83cb20e9d5c
Krisztian Szeles <krisztian.szeles@ilogics.com>
2021-02-17 13:07:05 +0100

    Introduce basesepeed constant

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -1,15 +1,16 @@
 package parrot;
 
 public abstract class  Parrot {
+
+    public static final double BASESPEED = 12.0;
+
     public Parrot() {
     }
 
     public abstract double getSpeed();
 
-
-
     protected double getBaseSpeed() {
-        return 12.0;
+        return BASESPEED;
     }
 
 }
