c6e11f48e9080087ce6bff4aece612dbcda888ea
pn13057 <anca0>
2021-02-17 13:19:32 +0100

    extract constant for max speed in norwegian

--- src/main/java/parrot/NorwegianParrot.java
+++ src/main/java/parrot/NorwegianParrot.java
@@ -2,6 +2,7 @@ package parrot;
 
 public class NorwegianParrot extends Parrot {
 
+    private static final double MAX_SPEED = 24.0;
     private final double voltage;
     private final boolean isNailed;
 
@@ -17,7 +18,7 @@ public class NorwegianParrot extends Parrot {
     }
 
     private double getBaseSpeed() {
-        return Math.min(24.0, voltage * BASE_SPEED);
+        return Math.min(MAX_SPEED, voltage * BASE_SPEED);
     }
 
 }
