f4df3d9a75a56da3c0afde8f806c44c3fbd56e36
pn13057 <anca0>
2021-02-17 11:23:16 +0100

    AfricanParrot

---
+++ src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+    public AfricanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
