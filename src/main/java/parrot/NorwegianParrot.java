cde26b1d3cf8e22f7eac6de2560bba1c1979f7a6
pn13057 <anca0>
2021-02-17 11:44:59 +0100

    move voltage and isNailed to norwegian

--- src/main/java/parrot/NorwegianParrot.java
+++ src/main/java/parrot/NorwegianParrot.java
@@ -1,8 +1,14 @@
 package parrot;
 
 public class NorwegianParrot extends Parrot {
+
+    protected double voltage;
+    protected boolean isNailed;
+
     public NorwegianParrot(double voltage, boolean isNailed) {
         super(voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     @Override
