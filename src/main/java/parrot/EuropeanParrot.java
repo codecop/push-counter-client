b59c38b12bcc2188e6c5eee2f9eeed6f6a57ee14
pn13057 <anca0>
2021-02-17 11:47:52 +0100

    remove constructor from european

--- src/main/java/parrot/EuropeanParrot.java
+++ src/main/java/parrot/EuropeanParrot.java
@@ -2,10 +2,6 @@ package parrot;
 
 public class EuropeanParrot extends Parrot{
 
-    public EuropeanParrot() {
-        super();
-    }
-
     @Override
     public double getSpeed() {
         return super.getBaseSpeed();
